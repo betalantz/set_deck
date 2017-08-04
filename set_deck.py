86e4f14dca7375f592488363dbff444786a5e316
betalantz <betalantz@gmail.com>
2017-08-03 17:29:21 -0700

    update

--- set_deck.py
+++ set_deck.py
@@ -67,7 +67,6 @@ class Deck(object):
 
   def addPlayer(self, player,):
     self.players.append(player)
-
     player.addDeck(self)
     return self
   def displayPlayers(self):
@@ -81,12 +80,14 @@ class Player(object):
     self.total = 0
     self.deck = ''
     self.set = []
+    self.selectedCards = []
   def addDeck(self, deck):
     self.deck = deck
   def info(self):
     pass
     # print info
 
+<<<<<<< HEAD
   def selectSet(self, a, b, c):
     self.selectedCards = []
     self.selectedCards.append(self.deck.activeCards[a-1])
@@ -95,6 +96,22 @@ class Player(object):
     self.activeCards.remove(a-1)
     self.activeCards.remove(b-1)
     self.activeCards.remove(c-1)
+=======
+  def selectSet(self, three_cards):
+    three_cards = list(three_cards)
+    three_cards.remove(')')
+    three_cards.remove(',')
+    three_cards.remove(',')
+    three_cards.remove('(')
+    three_cards = map(int, three_cards)
+    print three_cards
+    self.selectedCards.append(self.deck.activeCards[three_cards[0] - 1])
+    self.selectedCards.append(self.deck.activeCards[three_cards[1] - 1])
+    self.selectedCards.append(self.deck.activeCards[three_cards[2] - 1])
+    self.deck.activeCards.remove(self.deck.activeCards[three_cards[2] - 1])
+    self.deck.activeCards.remove(self.deck.activeCards[three_cards[1] - 1])
+    self.deck.activeCards.remove(self.deck.activeCards[three_cards[0] - 1])
+>>>>>>> 2f65d4b5070c52e00d56d4e996074ac3fcb52795
     return self
 
 
@@ -102,16 +119,17 @@ class Player(object):
     i = 0
     for card in self.selectedCards:
       i += 1
-
-    print "This is card",i,":", card["number"], card["shape"], card["color"], card["fill"]
+      print "This is card",i,":", card["number"], card["shape"], card["color"], card["fill"]
+    print '\n'
 
 deck1 = Deck('deck1')
 
 def startgame():
   val = raw_input('How many players? ')
   for i in range(0, int(val)):
-    name = raw_input('Enter name of Player ' + str(i) + " ")
+    name = raw_input('Enter name of Player ' + str(i + 1) + " ")
     deck1.addPlayer(Player(name))
+    print deck1.players[i].name
 
 # startgame()
 # deck1.displayActive()
@@ -125,5 +143,22 @@ player1 = Player('sfkl')
 deck1.displayActive()
 player1.selectSet(2,4,5).displaySet()
 deck1.displayActive()
+<<<<<<< HEAD
+=======
+while deck1.active:
+  val = raw_input('>>> ')
+  if val.lower() == 'quit':
+    deck1.active = False
+  elif val.lower() == 'check':
+    who = raw_input('Which player do you want to check? ')
+  elif val.lower() == 'grab':
+    who = raw_input('Which player is grabbing? ')
+    three_cards = raw_input('Which cards? ( , , ) ')
+    deck1.players[int(who) - 1].selectSet(three_cards)
+    deck1.players[int(who) - 1].displaySet()
+    deck1.displayActive()
+  elif val.lower() == 'scores':
+    deck1.displayPlayers
+>>>>>>> 2f65d4b5070c52e00d56d4e996074ac3fcb52795
 
 
---
+++ test.py
@@ -0,0 +1,8 @@
+three_cards = '(1,1,2)'
+three_cards = list(three_cards)
+three_cards.remove(')')
+three_cards.remove(',')
+three_cards.remove(',')
+three_cards.remove('(')
+# three_cards = map(int, three_cards)
+print three_cards
